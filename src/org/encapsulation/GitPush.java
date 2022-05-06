cd8fe58f9b770abcbb50867150b9e453c068a385
Suriya1050 <soorya0593@gmail.com>
2022-05-06 12:10:52 +0530

    2nd Push

---
+++ src/org/encapsulation/GitPush.java
@@ -0,0 +1,15 @@
+package org.encapsulation;
+
+public class GitPush {
+	
+	private void empId() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
