38b47d1f19955ea39565a70cc9e7bac0f05b55f7
Suriya1050 <soorya0593@gmail.com>
2022-05-06 11:56:09 +0530

    I pushed the code

---
+++ src/org/encapsulation/SbiBank.java
@@ -0,0 +1,18 @@
+package org.encapsulation;
+
+public class SbiBank {
+			
+	private int id;
+
+	public int getId() {
+		return id;
+		
+	}
+
+	public void setId(int id) {
+		this.id = id;
+
+		
+	}
+
+}
